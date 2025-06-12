693c124113b8b09be56844cb2c81c8a3e00c2b02
Ryan-Giard <giardr@oregonstate.edu>
2025-06-11 20:19:21 -0700

    Initial Commit

---
+++ terraform/main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "minecraft" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [aws_security_group.minecraft_sg.id]
+
+  tags = {
+    Name = "MinecraftServer"
+  }
+}
+
+resource "aws_security_group" "minecraft_sg" {
+  name        = "minecraft_sg"
+  description = "Allow Minecraft traffic"
+
+  ingress {
+    from_port   = 25565
+    to_port     = 25565
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress { 
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+}
